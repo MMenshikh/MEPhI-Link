32744c07571c89c76f4c8a87d69e8efc8ced20e3
MMenshikh <menshih.maksym@yandex.ru>
2025-12-05 05:05:38 +0300

    Reset database and Make user admin

--- backend/make-admin.py
+++ backend/make-admin.py
@@ -1,10 +1,25 @@
+# backend/make_admin.py
 import sqlite3
 
 conn = sqlite3.connect('mephi_link.db')
-cursor = conn.cursor()
+cur = conn.cursor()
 
-cursor.execute('UPDATE users SET role = ?, is_admin = 1 WHERE id = 1', ('admin',))
-conn.commit()
+email = "menshih.maksym@yandex.ru"
+
+cur.execute("SELECT id, first_name, last_name, role, is_admin FROM users WHERE email = ?", (email,))
+user = cur.fetchone()
+print("Найден пользователь:", user)
+
+if user:
+    cur.execute("""
+        UPDATE users
+        SET is_admin = 1,
+            role = 'admin'
+        WHERE email = ?
+    """, (email,))
+    conn.commit()
+    print("✅ Пользователь стал админом")
+else:
+    print("❌ Пользователь не найден")
+
 conn.close()
-
-print("✅ user_id=1 теперь админ с role='admin'")
